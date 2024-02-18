80e029f29e6dab1059ee609fb9c7236fd0216ef4
walternoguera <walter.morel.noguera@gmail.com>
2024-02-18 02:43:26 +0100

    refactorizando

--- intento4/agenda.py
+++ intento4/agenda.py
@@ -1,9 +1,17 @@
 import sqlite3 as lite
 
-conexion = lite.connect('agenda.sqlite')
-cursor = conexion.cursor()
-cursor.execute("SELECT * FROM usuarios;")
-resultado = cursor.fetchall()
-conexion.close()
+def db(query):
+    conexion = lite.connect('agenda.sqlite')
+    cursor = conexion.cursor()
+    cursor.execute(query)
+    resultado = cursor.fetchall()
+    conexion.close()
+    return resultado
 
-print(resultado)
+def menu():
+    opcion = input("elige una opcion de agenda:\n"+"1.listar\n"+
+                   "2.insertar")
+    if opcion == "1":
+        resultado = db("SELECT * FROM usuarios")
+        print(resultado)
+menu()
